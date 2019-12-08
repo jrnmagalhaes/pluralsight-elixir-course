561922fd4d62357381eb2932f014907dfabf830d
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-08 17:58:10 -0300

    :sparkles: config a cron job to send random twettes every minute

--- config/config.exs
+++ config/config.exs
@@ -4,5 +4,5 @@ config :pluralsight_tweet_course, PluralsightTweetCourse.Scheduler,
   debug_logging: false,
   jobs: [
     # Every minute
-    {"* * * * *", fn -> IO.puts PluralsightTweetCourse.FileReader.get_strings_to_tweet("lib/pluralsight_tweet_course/sample.txt") end}
+    {"* * * * *", fn -> PluralsightTweetCourse.Tweet.send_random("lib/pluralsight_tweet_course/sample.txt") end}
   ]
