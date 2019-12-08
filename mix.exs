fef1210d5a9df0253a54825dd0e9629779eed4c8
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-08 17:07:52 -0300

    :heavy_plus_sign: adds extwitter and oauth dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule PluralsightTweetCourse.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :quantum],
+      extra_applications: [:logger, :quantum, :extwitter],
       mod: {PluralsightTweetCourse.Application, []}
     ]
   end
@@ -22,7 +22,9 @@ defmodule PluralsightTweetCourse.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:quantum, "~> 2.3"}
+      {:quantum, "~> 2.3"},
+      {:extwitter, "~> 0.9.6"},
+      {:oauth, github: "tim/erlang-oauth"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
