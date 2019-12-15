810277ccb827d2ec5c4589f1be47acbb414503a4
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-15 17:11:41 -0300

    :art: refactoring FileReader module to not depend on file system for tests purpose

--- lib/pluralsight_tweet_course/file_reader.ex
+++ lib/pluralsight_tweet_course/file_reader.ex
@@ -2,6 +2,11 @@ defmodule PluralsightTweetCourse.FileReader do
     def get_strings_to_tweet(path) do
         File.read!(path)
         # the pipe operator |> passes the result of an expression as the first parameter of another expression.
+        |> pick_string
+    end
+
+    def pick_string(str) do
+        str
         |> String.split("\n") # Find the lines
         |> Enum.map(&String.trim/1) # Removes spaces in the end and in the beginning
         |> Enum.filter(&String.length(&1) <= 140) # Selects the lines that are have less than 140 caracters (The maximux on a tweet)
