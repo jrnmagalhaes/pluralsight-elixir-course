d82e86377598ca7d95a9526326a5cc1abfc4cacd
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-15 17:29:54 -0300

    :sparkles: adds a test case with mocked function

--- test/pluralsight_tweet_course/file_reader_test.exs
+++ test/pluralsight_tweet_course/file_reader_test.exs
@@ -1,5 +1,6 @@
 defmodule FileReaderTest do
   use ExUnit.Case
+  import Mock
 
   test "passing a file should return a string" do
     str = PluralsightTweetCourse.FileReader.get_strings_to_tweet(Path.join("#{:code.priv_dir(:pluralsight_tweet_course)}", "sample.txt"))
@@ -17,5 +18,13 @@ defmodule FileReaderTest do
     str = PluralsightTweetCourse.FileReader.pick_string("")
 
     assert str == "";
+  end
+
+  test "The string should be trimmed" do
+    with_mock File, [read!: fn(_) -> " ABC " end] do
+      str = PluralsightTweetCourse.FileReader.get_strings_to_tweet("some random file.txt")
+
+      assert str == "ABC"
+    end
   end
 end
