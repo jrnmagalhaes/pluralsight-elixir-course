af60a80a8f5db5f6be32ea66665f7a5dbe394bd9
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-15 16:58:40 -0300

    :art: using the priv directory convention to store assets

--- config/config.exs
+++ config/config.exs
@@ -4,5 +4,5 @@ config :pluralsight_tweet_course, PluralsightTweetCourse.Scheduler,
   debug_logging: false,
   jobs: [
     # Every minute
-    {"* * * * *", fn -> PluralsightTweetCourse.Tweet.send_random("lib/pluralsight_tweet_course/sample.txt") end}
+    {"* * * * *", fn -> PluralsightTweetCourse.Tweet.send_random(Path.join("#{:code.priv_dir(:pluralsight_tweet_course)}", "sample.txt")) end}
   ]
