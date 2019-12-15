8ec99c0580a25ace5d7b73819eab3daf38a1cbdf
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-15 17:10:30 -0300

    :sparkles: adding two test cases to FileReader module

---
+++ test/pluralsight_tweet_course/file_reader_test.exs
@@ -0,0 +1,15 @@
+defmodule FileReaderTest do
+  use ExUnit.Case
+
+  test "passing a file should return a string" do
+    str = PluralsightTweetCourse.FileReader.get_strings_to_tweet(Path.join("#{:code.priv_dir(:pluralsight_tweet_course)}", "sample.txt"))
+
+    assert str != nil
+  end
+
+  test "will not return a string longer than 140 characters" do
+    str = PluralsightTweetCourse.FileReader.get_strings_to_tweet(Path.join("#{:code.priv_dir(:pluralsight_tweet_course)}", "test/too_long.txt"))
+    
+    assert str == "short line."
+  end
+end
--- test/pluralsight_tweet_course_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PluralsightTweetCourseTest do
-  use ExUnit.Case
-  doctest PluralsightTweetCourse
-
-  test "greets the world" do
-    assert PluralsightTweetCourse.hello() == :world
-  end
-end
